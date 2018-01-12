931d1fde1e2768a7a96a78b5b9fa1deb1d4374ad
DANIEL PERVEZ - J497484 <J497484@inetpsa.com>
2018-01-12 17:11:51 +0100

    change menu behavior

--- js/creative.js
+++ js/creative.js
@@ -1,20 +1,20 @@
 (function($) {
     "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
+
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 110)
+            scrollTop: ($($anchor.attr('href')).offset().top - 0)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
 
     // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+    // $('body').scrollspy({
+    //     target: '.navbar-fixed-top',
+    //     offset: 51
+    // });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -22,11 +22,11 @@
     });
 
     // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+    // $('#mainNav').affix({
+    //     offset: {
+    //         top: 100
+    //     }
+    // })
 
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();

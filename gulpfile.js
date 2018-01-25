582448df1475fa0413e328cd70ba69679c56cc7a
DANIEL PERVEZ - J497484 <J497484@inetpsa.com>
2018-01-25 16:38:06 +0100

    add bootstrap less

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,53 @@ var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
+var del = require('del'); // rm -rf
 
-// Compile LESS files from /less into /css
-gulp.task('less', function() {
-    return gulp.src('less/bureau401.less')
-        .pipe(less())
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
+// refers to my build directory and or files to
+// to delete
+var toDelete = [
+    'dist',
+]
+
+// deletes files
+gulp.task('clean', function() {
+    return del(toDelete); // rm -rf
 });
 
-// Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/bureau401.css')
+// Compile LESS files from /less into /css
+// gulp.task('less', function() {
+//     return gulp.src('less/bureau401.less')
+//         .pipe(less())
+//         .pipe(gulp.dest('css'))
+//         .pipe(browserSync.reload({
+//             stream: true
+//         }))
+// });
+
+gulp.task('less', function() {
+
+    return new Promise(function(resolve, reject) {
+        gulp.src('less/bureau401.less')
+            .pipe(less())
+            .pipe(gulp.dest('css'));
+        gulp.src('less/bootstrap/bootstrap.less')
+            .pipe(less())
+            .pipe(gulp.dest('css'))
+            .pipe(browserSync.reload({
+                stream: true
+            }));
+        resolve();
+
+    })
+});
+
+gulp.task('minify-css', () => {
+    return gulp.src('css/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('css'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -39,33 +69,39 @@ gulp.task('minify-js', function() {
         }))
 });
 
-// Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
+    return new Promise(function(resolve, reject) {
+        gulp.src('node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map')
+            .pipe(gulp.dest('dist/bootstrap'));
 
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
+        gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+            .pipe(gulp.dest('dist/jquery'));
 
-    gulp.src(['node_modules/magnific-popup/dist/*'])
-        .pipe(gulp.dest('vendor/magnific-popup'))
+        gulp.src(['node_modules/magnific-popup/dist/*'])
+            .pipe(gulp.dest('dist/magnific-popup'))
 
-    gulp.src(['node_modules/scrollreveal/dist/*.js'])
-        .pipe(gulp.dest('vendor/scrollreveal'))
+        gulp.src(['node_modules/scrollreveal/dist/*.js'])
+            .pipe(gulp.dest('dist/scrollreveal'))
 
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        gulp.src([
+                'node_modules/font-awesome/**',
+                '!node_modules/font-awesome/**/*.map',
+                '!node_modules/font-awesome/.npmignore',
+                '!node_modules/font-awesome/*.txt',
+                '!node_modules/font-awesome/*.md',
+                '!node_modules/font-awesome/*.json'
+            ])
+            .pipe(gulp.dest('dist/font-awesome'))
+
+
+        resolve();
+    })
+
 })
 
 // Run everything
-gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default',
+    gulp.series('clean', 'less', 'minify-css', 'minify-js', 'copy'));
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -77,7 +113,7 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+gulp.task('dev', gulp.series('less', 'minify-css', 'minify-js', 'browserSync'), function() {
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
